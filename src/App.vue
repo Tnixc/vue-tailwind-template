b4ced2d4fb39dc6cfa5a6b4f6720799f9caeca5f
Tnixc <tnixxc@gmail.com>
2024-02-09 23:00:24 -0500

    enable css nesting

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <body class="bg-zinc-800 h-screen flex items-center justify-center flex-col">
     <div class="w-96 h-96 bg-teal-400/10 rounded-full fixed blur-3xl"></div>
-    <h1 class="text-white text-5xl font-bold">Vite + Vue + Tailwind</h1>
+    <h1 class=" text-5xl font-bold">Vite + Vue + Tailwind</h1>
   </body>
 </template>
